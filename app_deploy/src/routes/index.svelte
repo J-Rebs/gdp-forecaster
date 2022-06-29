95d7890748d63a4eb6e7727411e31c5f2c11617b
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2022-06-29 01:24:03 -0400

    deployment ready

---
+++ app_deploy/src/routes/index.svelte
@@ -0,0 +1,134 @@
+<script>
+	import * as d3 from "d3";
+	import { onMount } from "svelte";
+	import { LayerCake, Svg } from "layercake";
+	//import * as aq from "arquero";
+	//import * as aq from 'arquero';
+    
+	/**
+	 * Line Chart Components
+	 * Source: https://layercake.graphics/example/Line
+	 * */
+	import Line from "./lineChart/Line.svelte";
+	import Area from "./lineChart/Area.svelte";
+	import AxisXl from "./lineChart/AxisXL.svelte";
+	import AxisYl from "./lineChart/AxisYL.svelte";
+
+	let data;
+	let container;
+
+	const yKey = "WB_ny_gdp_mktp_cn_ad";
+	const xKey = "year";
+	/**
+	 * on mount permits loading of async data with later access
+	 * See: https://tuyukun.com/post/svelte_importdata/
+	 */
+	onMount(async () => {
+		//load and transform data
+		data = await d3.csv(
+			"https://raw.githubusercontent.com/J-Rebs/gdp-forecaster/main/data/country_forecast.csv"
+		);
+		data.forEach((e) => {
+			if (e[yKey] == "NA") {
+				e[yKey] = 0;
+			} else {
+				e[yKey] = parseFloat(e[yKey]);
+			}
+		});
+
+		//we also to create our arquero table
+		//const dt = aq.from(data);
+		//const child = document.createElement("div");
+		//child.innerHTML = dt.toHTML();
+		//container.appendChild(child);
+	});
+
+	$: {
+		console.log(data);
+	}
+</script>
+
+<main>
+	<h1>Rapid Data Vizualization</h1>
+	<p>
+		Rapid data visualization of csv data using 
+        <a href="https://layercake.graphics/">Laker Cake</a>,
+        <a href="https://d3js.org/">D3</a>, and 
+        <a href="https://svelte.dev/">Svelte</a>
+	</p>
+</main>
+
+<div class="chart-container">
+	<LayerCake
+		padding={{ right: 10, bottom: 20, left: 25 }}
+		x={xKey}
+		y={yKey}
+		yDomain={[0, null]}
+		{data}
+	>
+		<Svg>
+			<AxisXl />
+			<AxisYl ticks={4} />
+			<Line />
+			<Area />
+		</Svg>
+	</LayerCake>
+</div>
+
+<h3>Graph notes</h3>
+<p>
+    The above is US GDP (WB_ny_gdp_mktp_cn_ad) data.
+    Where data read "NA" for a given year, data points have been set to 0.
+</p>
+<h3>What is this page</h3>
+<p>
+    This webpage is a meant to demonstrate how you can quickly deploy a website with data visualizations. It was built and deployed in an evening. 
+    The idea is to demonstrate that data visualizations can be shared with many stakeholders in a short time with the only prerequisite being internet access.
+    It reads data from a GitHub hosted CSV file using D3 directly into the webpage and then renders it with Layer Cake.
+    The ovearll app is built in Svelte. It is deployed using <a href="https://www.netlify.com/">Netlify</a>.
+</p>
+<h3>Who made this webpage</h3>
+<p>
+    Joe Rebagliati <a href="https://www.linkedin.com/in/joe-rebagliati-4ab7a488/">LinkedIn</a>.
+    The GitHub repository for this webpage is <a href="https://github.com/J-Rebs/gdp-forecaster">here</a>.
+</p>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+        font-family: 'Courier New', Courier, monospace;
+	}
+
+	h1 {
+		color: #000000;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 100;
+        font-family: 'Courier New', Courier, monospace;
+	}
+
+    h3{
+        font-family: 'Courier New', Courier, monospace;
+    }
+
+    p{
+        font-family: 'Courier New', Courier, monospace;
+
+    }
+
+	.chart-container {
+		width: 100%;
+		height: 500px;
+		_background-color: rgba(0, 0, 0, 0.1);
+		float: left;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
